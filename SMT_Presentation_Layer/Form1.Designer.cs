3dcb0bd67a6dd36d59422a97f0df2241d1cd3833
SUHAS S RAO <suhassrao005@gmail.com>
2021-07-02 23:39:00 +0530

    Icon

--- SMT_Presentation_Layer/Form1.Designer.cs
+++ SMT_Presentation_Layer/Form1.Designer.cs
@@ -29,10 +29,20 @@ namespace SMT_Presentation_Layer
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SMT";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
