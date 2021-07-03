6d2df2cae0e994d830080910d5ad8294d90932ca
rakul <rakuldavid16@gmail.com>
2021-07-03 10:20:34 +0530

    Pay In and Out

---
+++ SMT_DataLayer/Repository/Enumeration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMT_DataLayer.Repository
+{
+    class Enumeration
+    {
+        public static Dictionary<int, string> Atributes = new Dictionary<int, string>()
+        {
+            {(int)Details.Creditor,"00000000-0000-0000-0000-000000000001" },
+            {(int)Details.Deptor,"00000000-0000-0000-0000-000000000002" }
+        };
+    }
+    enum Details {
+        Creditor=1,
+        Deptor=2,
+        Invoice=3,
+        PayIn_Out=4
+    }
+}
